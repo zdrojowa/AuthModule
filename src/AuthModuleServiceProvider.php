fa6a7070e122cdc4e46a4ea8ee6b0368c2240275
Jakub Kosiński <j.kosinski@zdrojowainvest.pl>
2019-08-23 16:39:44 +0200

    Service Provider Assets

--- src/AuthModuleServiceProvider.php
+++ src/AuthModuleServiceProvider.php
@@ -16,15 +16,7 @@ class AuthModuleServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadViews()->loadTranslations();
-    }
-
-    /**
-     *
-     */
-    public function register()
-    {
-
+        $this->loadViews()->loadTranslations()->publishAssets();
     }
 
     /**
@@ -46,11 +38,25 @@ class AuthModuleServiceProvider extends ServiceProvider
      */
     protected function loadTranslations(): AuthModuleServiceProvider
     {
-        $this->loadTranslationsFrom(__DIR__.'/../resources/lang/', 'AuthModule');
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang/', 'AuthModule');
 
         $this->publishes([
             __DIR__ . '/../resources/lang/' => resource_path('lang/vendor/' . 'AuthModule'),
         ], 'views');
+
+        return $this;
+    }
+
+    /**
+     * @return AuthModuleServiceProvider
+     */
+    protected function publishAssets(): AuthModuleServiceProvider
+    {
+        $this->publishes(([
+            __DIR__ . '/../resources/assets/js' => resource_path('js/vendor/AuthModule'),
+            __DIR__ . '/../resources/assets/sass' => resource_path('sass/vendor/AuthModule'),
+            __DIR__ . '/../resources/assets/img' => resource_path('assets/vendor/AuthModule'),
+        ]));
 
         return $this;
     }
